6a3c336d4d7f8485cd3066209497ce23291995ff
Rleahy22 <ryanleahy90@gmail.com>
2013-07-03 11:32:22 -0500

    Update application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-  var rand = 1 + Math.floor(Math.random() * 9);
-
   $('#get_color').click(function(event) {
     event.preventDefault();
-    console.log(rand);
-    $.post('/color', function(results) {
-      $('#color_me li:nth-child(' + rand + ')').css({"backgroundColor": results});
+    $.getJSON('/color', function(data) {
+      console.log(data);
+      $('#color_me li:nth-child(' +data.cell+ ')').css({"backgroundColor": data.color});
     });
   });
 });
+
